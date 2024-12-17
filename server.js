98a9080a92d83f8ba50dc909d2f54da8d837bb39
Layla Abdulrahman <laylaaalaylaaa17@gmail.com>
2024-12-17 18:37:49 +0200

    Add root route to fix 404 error

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 //mongodb
 require('./config/db');
 
@@ -10,11 +12,18 @@ const UserRouter = require("./api/User");
 const bodyParser = require("express").json;
 app.use(bodyParser());
 
+app.use(express.json());
+
  // cors accesss to enable application on different hosts to send request
-    const cors = require("cors");
-    app.use(cors());
+const cors = require("cors");
+app.use(cors());
 
 app.use('/user', UserRouter)
+
+// Root route to avoid 404 errors
+app.get("/", (req, res) => {
+    res.send("Welcome to TriviaTwist API!");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
